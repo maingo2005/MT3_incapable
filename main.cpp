70a6a37145a68076770efe3bdd1534b3978296e7
maingo2005 <147121658+maingo2005@users.noreply.github.com>
2025-01-30 12:52:09 +0900

    初期化

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
 #include <Novice.h>
 
-//#include "../MT3.h"
-#include <imgui.h>
-
-const char kWindowTitle[] = "MT3_03_00_Basic";
+const char kWindowTitle[] = "GC2C_12_マインゴ_シズカ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	const int kWindowWidth = 1280;
-	const int kWindowHeight = 720;
-	const float kAxesSize = 100.0f;
-	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-
-	Vector3 controlPoints[3] = {
-	  {-0.8f, 0.58f, 1.0f },
-	  {1.76f, 1.0f,  -0.3f},
-	  {0.94f, -0.7f, 2.3f },
-	};
-
-	Camera camera{};
-	camera.spherical.theta = 0.26f;
-	camera.spherical.radius = 7.0f;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -41,28 +25,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Update(camera);
-
-		ImGui::Begin("Window");
-
-		ImGui::DragFloat3("controlPoints[0]", &controlPoints[0].x, 0.01f);
-		ImGui::DragFloat3("controlPoints[1]", &controlPoints[1].x, 0.01f);
-		ImGui::DragFloat3("controlPoints[2]", &controlPoints[2].x, 0.01f);
-
-		ImGui::End();
-
-		Matrix4x4 viewMatrix = CalcViewMatrix(camera);
-		Matrix4x4 projectionMatrix =
-		  MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
-
-		// ViewportMatrixを作る
-		Matrix4x4 viewportMatrix =
-		  MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-
-		Matrix4x4 viewportMatrixForAxes = MakeViewportMatrix(
-		  0.0f, float(kWindowHeight) - kAxesSize, kAxesSize, kAxesSize, 0.0f, 1.0f);
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -70,14 +32,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
-		// 描画
-		DrawGrid(viewProjectionMatrix, viewportMatrix);
-		DrawBezier(
-		  controlPoints[0], controlPoints[1], controlPoints[2], viewProjectionMatrix,
-		  viewportMatrix, BLUE);
-
-		DrawAxis(camera, viewportMatrixForAxes);
 
 		///
 		/// ↑描画処理ここまで
