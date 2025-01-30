1fd0db5f877c0f0c0df37e9f76293eab9e79f56f
maingo2005 <147121658+maingo2005@users.noreply.github.com>
2025-01-30 12:49:43 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "GC2C_12_マインゴ_シズカ";
+//#include "../MT3.h"
+#include <imgui.h>
+
+const char kWindowTitle[] = "MT3_03_02_Basic";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+	const float kAxesSize = 100.0f;
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Camera camera{};
+	camera.spherical.theta = 0.26f;
+	camera.spherical.radius = 7.0f;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +35,48 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Update(camera);
+
+		Vector3 a{0.2f, 1.0f, 0.0f};
+		Vector3 b{2.4f, 3.1f, 1.2f};
+
+		Vector3 c = a + b;
+		Vector3 d = a - b;
+		Vector3 e = a * 2.4f;
+
+		Vector3 rotate{0.4f, 1.43f, -0.8f};
+		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+		Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
+
+		ImGui::Begin("Window");
+
+		ImGui::Text("c:%f, %f, %f", c.x, c.y, c.z);
+		ImGui::Text("d:%f, %f, %f", d.x, d.y, d.z);
+		ImGui::Text("e:%f, %f, %f", e.x, e.y, e.z);
+
+		ImGui::Text(
+		  "matrix:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n",
+		  rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
+		  rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
+		  rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
+		  rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
+
+		ImGui::End();
+
+		Matrix4x4 viewMatrix = CalcViewMatrix(camera);
+		Matrix4x4 projectionMatrix =
+		  MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix4x4 viewProjectionMatrix = viewMatrix * projectionMatrix;
+
+		// ViewportMatrixを作る
+		Matrix4x4 viewportMatrix =
+		  MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		Matrix4x4 viewportMatrixForAxes = MakeViewportMatrix(
+		  0.0f, float(kWindowHeight) - kAxesSize, kAxesSize, kAxesSize, 0.0f, 1.0f);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +84,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		// 描画
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+
+		DrawAxis(camera, viewportMatrixForAxes);
 
 		///
 		/// ↑描画処理ここまで
